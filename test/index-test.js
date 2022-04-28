926cc3598fa006a721630bd3ae4afc9861066924
Stefanny Al <stefannydefreitas@gmail.com>
2022-04-28 10:09:10 -0400

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -73,3 +73,37 @@ describe('preserveDecimal(n)', function() {
     expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
   })
 })
+
+function add(a,b){
+return a+b
+}
+
+function subtract(a,b){
+  return a-b
+
+}
+
+function multiply(a,b){
+return a*b
+}
+
+function divide(a,b) {
+return a/b
+}
+
+function increment(a){
+  return a+1
+
+}
+
+function decrement(a){
+return a-1
+}
+
+function makeInt(a){
+  return parseInt (a,10)
+}
+
+function preserveDecimal(a){
+  return parseFloat(a,10)
+}
